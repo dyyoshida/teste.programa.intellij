aa001da8090694146b2c4020bcf2fbd291c05fb8
Jinho <dyyoshida@gmail.com>
2022-01-14 17:19:25 -0300

    first commit

---
+++ src/br/com/dio/primeiroprograma.java
@@ -0,0 +1,21 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class primeiroprograma {
+    public static void main(String[] args) {
+
+        gato gato = new gato();
+
+        System.out.println(gato);
+
+
+        /*
+        int a = 5;
+        int b = 10;
+        System.out.println("Seja bem vindo !! " + (a+b));
+*/
+
+    }
+}
+
